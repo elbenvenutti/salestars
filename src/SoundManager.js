aec651156b42e4cdf13f0eb34535ddbad87e2371
travis-ci <travis@travis-ci.org>
2016-12-19 19:37:37 +0000

    sexier santa voice

--- src/SoundManager.js
+++ src/SoundManager.js
@@ -4,7 +4,7 @@ var Howl = require('howler').Howl;
 
 module.exports = class {
     constructor() {
-        this.purchaseSound = new Howl({ urls: [ 'santa/daSanta.mp3' ] });
+        this.purchaseSound = new Howl({ urls: [ 'santa/HoHoHo.mp3' ] });
         this.bellSound = new Howl({ urls: [ 'santa/bells.mp3' ] });
     }
 
