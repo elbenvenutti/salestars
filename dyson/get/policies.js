ebf1f6340e16a187dfa7e98f3267eed55f578b3f
Isma Perez <pb.ismael@gmail.com>
2016-12-20 12:12:09 +0000

    small fixes

--- dyson/get/policies.js
+++ dyson/get/policies.js
@@ -15,12 +15,12 @@ module.exports = {
             premium: () => faker.finance.amount(100, 500, 2, '£ '),
             postcode: () => faker.random.arrayElement(['EC3A 7LP', 'SW1A 1AA', 'N5 1BU', 'SW6 1HS', 'EC3N 4AB']),
             number_of_quotes: () => 1 + faker.random.number(10),
-            event: () => ([
-                faker.random.number(100) > 70 ? 'new business purchase' : 'other1',
-                faker.random.number(100) > 90 ? 'new business cancel' : 'other2',
-                faker.random.number(100) > 70 ? 'renewal purchase' : 'other3',
-                faker.random.number(100) > 80 ? 'renewal cancel' : 'other4'
-            ]),
+            event: () => (
+                faker.random.number(100) > 70 ? 'new business purchase' :
+                faker.random.number(100) > 90 ? 'new business cancel' :
+                faker.random.number(100) > 70 ? 'renewal purchase' :
+                faker.random.number(100) > 80 ? 'renewal cancel' : 'new business'
+            ),
             created: () => moment().subtract(faker.random.number((query.minutes || 1) * 60), 'seconds')
         };
     }
